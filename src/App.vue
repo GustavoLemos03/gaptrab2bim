bb1948e200dd20db9e123414eeefab1d298a8c39
Letícia Alves <lealvesoliveira55@gmail.com>
2023-06-05 14:13:07 -0300

    tela frutas

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app id="inspire">
+
+   
+    <router-view />
+    <v-app-bar class="px-3" color="white" flat density="compact">
+      
+
+      <v-spacer></v-spacer>
+
+      <v-tabs centered color="grey-darken-2">
+        <v-tab> <router-link to="/frutas">Estoque de Frutas</router-link>
+
+        </v-tab>
+        <v-tab> <router-link to="/cadastro">Cadastre-se</router-link>
+
+        </v-tab>
+        <v-tab> <router-link to="/compras">Comprar</router-link>
+
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+
+     
+    </v-app-bar>
+
+
+
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    //HelloWorld,
+  },
+
+  data: () => ({
+    
+  }),
+}
+</script>
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import frutas from '../view/frutas_view.vue'
+import home from '../view/home_view.vue'
+const routes = [
+  {
+    path: '/frutas',
+    name: 'frutas',
+    component: frutas
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: home
+  },
+  
+  
+]
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+export default router
