c8c878721d3dc850acc39605fa7a79968cd3a877
GustavoLemos03 <129064673+GustavoLemos03@users.noreply.github.com>
2023-06-07 22:23:36 -0300

    Alteração no pesquisar

--- src/view/cliente_view.vue
+++ src/view/cliente_view.vue
@@ -5,45 +5,27 @@
             <v-row>
 
                 <v-col cols="12" sm="12">
-                    <h1 class="center">Clientes</h1>
-                    <v-sheet min-height="70vh" rounded="lg" class="pa-12">
+
+                    <v-sheet min-height="100vh" rounded="lg" class="pa-12">
 
                         <v-row justify="space-around">
-
-                            <v-col cols="5">
-                                <v-form ref="form">
-                                    <v-text-field v-model="item" :rules="nameRules" label="Nome da fruta"
-                                        required></v-text-field>
-
-
-                                    <v-text-field v-model="qtd" :rules="qtdRules" label="Quantidade"
-                                        required></v-text-field>
-
-
-
-                                    <div class="d-flex flex-column">
-                                        <v-btn color="success" class="mt-4" block @click="validate">
-                                            Cadastrar
-                                        </v-btn>
-
-                                        <v-btn color="error" class="mt-4" block @click="reset">
-                                            Cancelar
-                                        </v-btn>
-
-                                    </div>
-
-                                </v-form>
-                            </v-col>
-
-                            <v-col cols="5" v-if="lista.length != 0">
-                                <v-card width="400" v-for="i in lista" :key="i" class="bg-grey-lighten-3">
+                            <v-col cols="5" v-if="cliente.length != 0">
+                                <v-card width="400" v-for="i in cliente" :key="i" class="ma-3 bg-grey-lighten-3">
                                     <div class="d-flex flex-no-wrap justify-space-between">
                                         <div>
                                             <v-card-title class="text-h5">
-                                                {{ i.item }}
+                                                {{ i.nomecliente }}
                                             </v-card-title>
 
-                                            <v-card-subtitle> Quantidade: {{ i.qtd }}</v-card-subtitle>
+                                            <v-card-text>
+                                                <p>CPF: {{ i.cpf }}</p>
+                                                <p>email: {{ i.email }}</p>
+                                                <p>Telefone: {{ i.telefone }}</p>
+                                                <p>Rua: {{ i.rua }} N°: {{ i.numero }}</p>
+                                                <p>Cidade: {{ i.cidade }}</p>
+                                            </v-card-text>
+
+
 
                                             <v-card-actions>
                                                 <v-btn class="ms-2" icon="mdi-delete" variant="text"
@@ -58,16 +40,16 @@
                                     </div>
                                 </v-card>
                             </v-col>
-                            <v-col cols="5" v-if="lista.length == 0">
-                                <v-card width="400" class="bg-grey-lighten-3">
+                            <v-col cols="5" v-if="cliente.length == 0">
+                                <v-card width="400" class="ma-3 bg-grey-lighten-3">
                                     <div class="d-flex flex-no-wrap justify-center">
                                         <div>
                                             <v-card-title class="text-h5">
-                                                Estoque vazio!
+                                                Nenhum cliente cadastrado!
                                             </v-card-title>
 
                                             <v-avatar class="ma-3" size="125" rounded="0">
-                                                <v-img src="../../public/vazio.png"></v-img>
+                                                <v-img src="../../public/cliente.png"></v-img>
                                             </v-avatar>
 
                                         </div>
@@ -75,7 +57,76 @@
 
                                     </div>
                                 </v-card>
+
+
+
+                                <div class="ml-7 mt-15">
+                                    <h3> Pesquisar </h3>
+                                <v-row>
+                                    <v-col cols="10">
+                                        <v-form ref="form">
+                                            <v-text-field v-model="pesq" label="Nome do cliente" variant="underlined"
+                                                required id="pesq"></v-text-field>
+
+
+                                        </v-form>
+
+                                    </v-col>
+                                    <v-col cols="auto">
+                                        <v-btn icon="mdi-magnify" size="small" color="primary" @click="pesquisar()"></v-btn>
+                                    </v-col>
+                                </v-row>
+
+                                <p v-if="found.length != 0">{{ (found.nomecliente) }}/ CPF: {{ found.cpf }}</p>
+                                <p v-if="noresults">Nenhum cliente cadastrado com esse nome</p>
+                                </div>
+
+
+
                             </v-col>
+
+                            <v-col cols="5">
+                                <v-form ref="form">
+                                    <v-text-field v-model="nomecliente" :rules="nameRules" label="Nome do cliente:"
+                                        required></v-text-field>
+
+                                    <v-text-field v-model="cpf" :rules="nameRules" label="CPF:" required></v-text-field>
+
+                                    <v-text-field v-model="email" :rules="nameRules" label="Email:" required></v-text-field>
+
+                                    <v-text-field v-model="telefone" :rules="nameRules" label="Telefone:"
+                                        required></v-text-field>
+
+
+                                    <v-row>
+                                        <v-col> <v-text-field v-model="rua" :rules="nameRules" label="Rua:"
+                                                required></v-text-field></v-col>
+                                        <v-col>
+                                            <v-text-field v-model="numero" :rules="nameRules" label="Número:"
+                                                required></v-text-field>
+                                        </v-col>
+                                    </v-row>
+
+                                    <v-text-field v-model="cidade" :rules="nameRules" label="Cidade:"
+                                        required></v-text-field>
+
+                                    <div class="d-flex flex-column">
+                                        <v-btn color="success" class="mt-4" block @click="validate">
+                                            Cadastrar
+                                        </v-btn>
+
+                                        <v-btn color="error" class="mt-4" block @click="reset">
+                                            Cancelar
+                                        </v-btn>
+
+                                    </div>
+
+                                </v-form>
+
+                            </v-col>
+
+
+
                         </v-row>
 
 
@@ -95,51 +146,82 @@ export default {
         nameRules: [
             v => !!v || 'Preenchimento obrigatório',
         ],
-        qtdRules: [
-            v => !!v || 'Preenchimento obrigatório',
-        ],
+        nomecliente: '',
+        cpf: '',
+        email: '',
+        telefone: '',
+        rua: '',
+        cidade: '',
+        numero: '',
 
-        item: '',
-        qtd: '',
-        link: '',
         alter: false,
-        lista: [],
-        cod: ''
+        cliente: [],
+        cod: '',
+        pesq: '',
+        found: [],
+        noresults: false
     }),
 
     methods: {
-
+        save() {
+            const parsed = JSON.stringify(this.cliente);
+            localStorage.setItem('cliente', parsed);
+        },
         addList() {
 
-            if (this.qtd != '' && this.item != '') {
-                if (this.alter == false) {
-                    this.lista.push({ item: this.item, qtd: this.qtd, cod: this.lista.length + 1 })
-                } else {
-                    var index_edit = this.lista.findIndex((x) => x.cod === this.cod);
-                    this.lista[index_edit] = { cod: this.cod, item: this.item, qtd: this.qtd }
-                    this.alter = true;
-                }
 
+            if (this.alter == false) {
+                this.cliente.push({ cod: this.cliente.length + 1, nomecliente: this.nomecliente, cpf: this.cpf, email: this.email, telefone: this.telefone, rua: this.rua, numero: this.numero, cidade: this.cidade })
+
+                this.save();
+            } else {
+                var index_edit = this.cliente.findIndex((x) => x.cod === this.cod);
+                this.cliente[index_edit] = { cod: this.cliente.length + 1, nomecliente: this.nomecliente, cpf: this.cpf, email: this.email, telefone: this.telefone, rua: this.rua, numero: this.numero, cidade: this.cidade }
+                this.alter = true;
+                this.save();
             }
-            this.item = null
-            this.qtd = null
+
+            this.nomecliente = null;
+            this.cpf = null;
+            this.email = null;
+            this.rua = null;
+            this.telefone = null;
+            this.numero = null;
+            this.cidade = null;
+
+
+
         },
         removeItem(index) {
-            var index_remove = this.lista.findIndex((x) => x.cod === index);
+            var index_remove = this.cliente.findIndex((x) => x.cod === index);
 
-            this.lista.splice(index_remove, 1)
-
+            this.cliente.splice(index_remove, 1)
+            this.save();
         },
         edit(index) {
             this.alter = true;
             this.cod = index;
 
-            var index_edit = this.lista.findIndex((x) => x.cod === index);
-            this.cod = index;
-            this.item = this.lista[index_edit].item;
-            this.qtd = this.lista[index_edit].qtd;
+            var index_edit = this.cliente.findIndex((x) => x.cod === index);
 
+            this.nomecliente = this.cliente[index_edit].nomecliente;
+            this.cpf = this.cliente[index_edit].cpf;
+            this.telefone = this.cliente[index_edit].telefone;
+            this.email = this.cliente[index_edit].email;
+            this.rua = this.cliente[index_edit].rua;
+            this.numero = this.cliente[index_edit].numero;
+            this.cidade = this.cliente[index_edit].cidade;
+        },
+        pesquisar() {
+            var search = this.cliente.find(e => e.nomecliente == this.pesq);
 
+            if (search != undefined) {
+                this.found = search
+                this.noresults = false
+            } else {
+                this.noresults = true
+                this.found = []
+            }
 
         },
         async validate() {
@@ -156,3 +238,9 @@ export default {
     },
 }
 </script>
+
+<style>
+#pesq {
+    text-transform: uppercase;
+}
+</style>
