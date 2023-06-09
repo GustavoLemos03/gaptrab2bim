fd7ffafbc1d7cf7fd5f1d4cf9e6a4238c896fd9f
Letícia Alves <lealvesoliveira55@gmail.com>
2023-06-08 22:09:25 -0300

    tela encomendas

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,15 @@
       <v-spacer></v-spacer>
 
       <v-tabs centered color="grey-darken-2">
-        <v-tab> <router-link to="/">Home</router-link> </v-tab>
+        <v-tab to="/"> <router-link to="/">Home</router-link> </v-tab>
 
-        <v-tab> <router-link to="/frutas">Estoque de Frutas</router-link>
-
-        </v-tab>
-        <v-tab> <router-link to="/cadastro">Clientes</router-link>
+        <v-tab to="/frutas"> <router-link to="/frutas">Estoque de Frutas</router-link>
 
         </v-tab>
-        <v-tab> <router-link to="/encomendas">Encomendas</router-link>
+        <v-tab to="/cadastro"> <router-link to="/cadastro">Clientes</router-link>
+
+        </v-tab>
+        <v-tab to="/encomendas"> <router-link to="/encomendas">Encomendas</router-link>
 
         </v-tab>
       </v-tabs>
